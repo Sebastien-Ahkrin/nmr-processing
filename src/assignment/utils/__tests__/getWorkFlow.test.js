6a1fc4b4cc0fb26b63b679a078a7f8bedbd7be91
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-07-16 12:58:09 +0200

    chore(autoAssignment): add getWorkFlow function

---
+++ src/assignment/utils/__tests__/getWorkFlow.test.js
@@ -0,0 +1,48 @@
+import getWorkFlow from '../getWorkFlow';
+import correlations from '../../data/correlations';
+
+describe('getWorkflow', () => {
+  it('simple test case', () => {
+    const result = getWorkFlow(correlations);
+    const desired = {
+      assignmentOrder: [['C'], ['H']],
+      experimentTypes: {
+        types: ["C", "H", "hmbc", "hsqc"],
+        hasProton: true,
+        hascarbon: true,
+        hasHSQC: true,
+      },
+    };
+    result.experimentTypes.types.sort();
+    expect(result).toStrictEqual(desired);
+  });
+  it('with justAssign', () => {
+    const result = getWorkFlow(correlations, ["C"]);
+    const desired = {
+      assignmentOrder: [['C']],
+      experimentTypes: {
+        types: ["C", "H", "hmbc", "hsqc"],
+        hasProton: true,
+        hascarbon: true,
+        hasHSQC: true,
+      },
+    };
+    console.log(result)
+    result.experimentTypes.types.sort();
+    expect(result).toStrictEqual(desired);
+  });
+  it('with justAssign', () => {
+    const result = getWorkFlow(correlations, ["C", 'H', 'N']);
+    const desired = {
+      assignmentOrder: [['C', 'H']],
+      experimentTypes: {
+        types: ["C", "H", "hmbc", "hsqc"],
+        hasProton: true,
+        hascarbon: true,
+        hasHSQC: true,
+      },
+    };
+    result.experimentTypes.types.sort();
+    expect(result).toStrictEqual(desired);
+  });
+});
