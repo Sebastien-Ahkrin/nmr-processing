a6cba6116453c9a4956d0665c297d07f7c5f92a7
Luc Patiny <luc@patiny.com>
2021-08-29 10:00:30 +0200

    feat: starting resurrect code

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@
  * @property {Array<Number>} y Array of y values
  */
 
+export * from './constants/gyromagneticRatio';
+export * from './constants/impurities';
+export * from './constants/couplingPatterns';
+
 export * from './peaks/peaksFilterImpurities';
-export * from './xy/xyAutoPeaksPicking';
-export * from './xy/xyAutoRangesPicking';
-export * from './signals/signalsToRanges';
-export * from './signals/signalsToXY';
-export * from './signals/signals2DToZ';
-export * from './ranges/rangesToACS';
+
 export * from './prediction/predictProton';
 export * from './prediction/predictCarbon';
 export * from './prediction/predictCOSY';
 export * from './prediction/predictHSQC';
 export * from './prediction/predictHMBC';
 export * from './prediction/predictAll';
+
+export * from './ranges/rangesToACS';
+
+export * from './signals/signalsToRanges';
+export * from './signals/signalsToXY';
+export * from './signals/signals2DToZ';
+
+export * from './utilities/resurrect';
+
+export * from './xy/xyAutoPeaksPicking';
+export * from './xy/xyAutoRangesPicking';
+
 export * from './xyz/xyzAutoPeaksPicking';
-export * from './constants/gyromagneticRatio';
-export * from './constants/impurities';
-export * from './constants/couplingPatterns';
 export * from './xyz/xyzJResAnalyzer';
