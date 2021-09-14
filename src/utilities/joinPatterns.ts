509dd3d8eb33261122b09661ad492955b9ef7586
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-09-14 15:49:25 +0200

    feat!: migration and refactor of joinPatterns

--- src/utilities/joinPatterns.ts
+++ src/utilities/joinPatterns.ts
@@ -1,19 +1,24 @@
 import { couplingPatterns } from '../constants/couplingPatterns';
 import { couplingValues } from '../constants/couplingValues';
 
-type couplingPatternKeys = keyof typeof couplingValues;
-const getKeyValue = <T extends object, U extends keyof T>(obj: T, key: U) => obj[key];
-
 /**
  *
  * @param {array<string>} patterns
  * @returns
  */
-export function joinPatterns(patterns: couplingPatternKeys[]) {
-  let sum = 0;
-  for (let pattern of patterns) {
-    sum += getKeyValue(couplingValues, pattern);
+export function joinPatterns(patterns: string[]) {
+  const sum = patterns.reduce((sum, pattern) => {
+    if (!couplingValues[pattern]) {
+      throw new Error(
+        `pattern ${pattern} is not in ${Object.keys(couplingValues).join(' ')}`,
+      );
+    }
+    return sum + couplingValues[pattern];
+  }, 0);
+
+  if (!couplingPatterns[sum]) {
+    throw new Error("The joined pattern doesn't exist");
   }
+
   return couplingPatterns[sum];
 }
-
