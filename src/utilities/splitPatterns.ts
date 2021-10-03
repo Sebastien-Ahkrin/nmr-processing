39ff98651c3fbf9004a3c99b1c10139ce2873929
Luc Patiny <luc@patiny.com>
2021-10-03 11:29:25 +0200

    fix: splitPatterns returns empty array if empty string

--- src/utilities/splitPatterns.ts
+++ src/utilities/splitPatterns.ts
@@ -1,4 +1,5 @@
 export function splitPatterns(multiplet: string) {
+  if (/^\s*$/.exec(multiplet)) return [];
   let result = multiplet.match(
     / *(quint|hex|sept|hept|oct|nona|non|s|d|t|q|h|o|n) */g,
   );
