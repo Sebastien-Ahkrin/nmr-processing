d87c1c8f3de824bdd417560d33ff49c6c190bb3f
Luc Patiny <luc@patiny.com>
2021-10-03 12:30:14 +0200

    feat: add getDatabase

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,32 @@
  * @property {Array<Number>} y Array of y values
  */
 
- export * from './constants/gyromagneticRatio';
- export * from './constants/impurities';
- export * from './constants/couplingPatterns';
+export * from './constants/gyromagneticRatio';
+export * from './constants/impurities';
+export * from './constants/couplingPatterns';
 
- export * from './peaks/peaksFilterImpurities';
+export * from './peaks/peaksFilterImpurities';
 
- export * from './prediction/predictProton';
- export * from './prediction/predictCarbon';
- export * from './prediction/predictCOSY';
- export * from './prediction/predictHSQC';
- export * from './prediction/predictHMBC';
- export * from './prediction/predictAll';
+export * from './prediction/predictProton';
+export * from './prediction/predictCarbon';
+export * from './prediction/predictCOSY';
+export * from './prediction/predictHSQC';
+export * from './prediction/predictHMBC';
+export * from './prediction/predictAll';
 
- export * from './ranges/rangesToACS';
- export * from './ranges/rangesToXY';
+export * from './ranges/rangesToACS';
+export * from './ranges/rangesToXY';
 
- export * from './signals/signalsToRanges';
- export * from './signals/signalsToXY';
- export * from './signals/signals2DToZ';
+export * from './signals/signalsToRanges';
+export * from './signals/signalsToXY';
+export * from './signals/signals2DToZ';
 
- export * from './utilities/resurrect';
+export * from './utilities/resurrect';
 
- export * from './xy/xyAutoPeaksPicking';
- export * from './xy/xyAutoRangesPicking';
+export * from './xy/xyAutoPeaksPicking';
+export * from './xy/xyAutoRangesPicking';
 
- export * from './xyz/xyzAutoPeaksPicking';
- export * from './xyz/xyzJResAnalyzer';
+export * from './xyz/xyzAutoPeaksPicking';
+export * from './xyz/xyzJResAnalyzer';
+
+export * from './utilities/getDatabase';
---
+++ src/utilities/__tests__/getDatabase.test.ts
@@ -0,0 +1,45 @@
+import { getDatabase } from '../getDatabase';
+
+test('getDatabase', async () => {
+  const database = await getDatabase();
+
+  expect(database).toHaveLength(2);
+  expect(database[0].ranges).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "from": 1.2,
+        "integration": 6,
+        "signals": Array [
+          Object {
+            "delta": 1.2,
+            "js": Array [
+              Object {
+                "coupling": 7,
+                "multiplicity": "t",
+              },
+            ],
+            "multiplicity": "t",
+          },
+        ],
+        "to": 1.2,
+      },
+      Object {
+        "from": 1.5,
+        "integration": 2,
+        "signals": Array [
+          Object {
+            "delta": 1.5,
+            "js": Array [
+              Object {
+                "coupling": 7,
+                "multiplicity": "hept",
+              },
+            ],
+            "multiplicity": "hept",
+          },
+        ],
+        "to": 1.5,
+      },
+    ]
+  `);
+});
