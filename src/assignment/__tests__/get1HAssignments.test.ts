115ca53369ccfc454222aa653fa088d8f83988d0
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-11-03 09:57:00 +0100

    chore: add ethylvinylether test case autoassignment

--- src/assignment/__tests__/get1HAssignments.test.ts
+++ src/assignment/__tests__/get1HAssignments.test.ts
@@ -3,6 +3,7 @@ import OCL from 'openchemlib';
 import { get1HAssignments } from '../get1HAssignments';
 
 import ethylbenzeneRange from './data/ethylbenzeneRanges.json';
+import ethylvinyletherRange from './data/ethylvinyletherRanges.json';
 
 describe('get1HAssignments', () => {
   it('ethylbenzene', async () => {
@@ -18,4 +19,17 @@ describe('get1HAssignments', () => {
     });
     expect(assignments).toHaveLength(2);
   });
+  it('ethylvinylether', async () => {
+    const molecule = OCL.Molecule.fromSmiles('CCOC=C');
+    const assignments = await get1HAssignments(ethylvinyletherRange, molecule, {
+      minScore: 0.1,
+      maxSolutions: 10,
+      restrictionByCS: {
+        tolerance: 1,
+        useChemicalShiftScore: true,
+        chemicalShiftRestriction: true,
+      },
+    });
+    expect(assignments).toHaveLength(5);
+  })
 });
